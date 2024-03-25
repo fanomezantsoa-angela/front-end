94c71a8255fcf2c2a983c8933dac38599ba0a2aa
fanomezantsoa-angela <fanomezantsoavonjiniainaangela@gmail.com>
2024-03-25 21:42:00 +0300

    modified:   e-commerce-front/src/index.css 	modified:   e-commerce-front/src/pages/Landingpages.jsx

--- e-commerce-front/src/pages/Landingpages.jsx
+++ e-commerce-front/src/pages/Landingpages.jsx
@@ -1,10 +1,25 @@
 import Inscription from "../components/Inscriptionfrom";
+import { Inputhandler } from "../Hooks/Inputhandler";
+
+import { useState, useEffect } from "react";
 function LandingPage() {
+  
     return (
       <div className="container">
         <div className="header-container">
-            <img src="" alt="" />
+          <img src="./src/assets/socolait.svg" alt="" />
         </div>
+        <p>
+          Nous partageons <br /> le gout du vrai.
+        </p>
+        <p>
+          Un pays aussi pur ne peut que <br /> produire le meilleur le lait
+        </p>
+        <input type="text" />
+        <button>
+          <img src="./src/assets/recherche.svg" alt="" />
+        </button>
+        <button>se connecter</button>
       </div>
     );
 }
