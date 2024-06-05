3c0ec6c2b6d9d42ed615b93b827f0b9d85b3c24e
PyWendi <rakotondranaivogilbert21@gmail.com>
2024-06-05 09:52:15 +0300

    Setting product component

--- e-commerce-front/src/components/AdminComponents/sections/ProductSection.jsx
+++ e-commerce-front/src/components/AdminComponents/sections/ProductSection.jsx
@@ -1,11 +1,18 @@
 import ProductComponent from "./childCOmponent/ProductComponent"
 import { getAllProduct } from "../../../actions/AdminProductAction"
 import { useState, useEffect } from "react"
+import Tooltip from '@mui/material/Tooltip';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 
 
 
 export default function ProductSection() {
-    const [customArray, setCustomArray] = useState([])
+    const [customArray, setCustomArray] = useState([1,1,1,1,1,1,11,1,2,1,11])
+
+    const addOperation = async () => {
+        alert("add product clicked")
+    }
+
 
     const fetchData = async () => {
         const data = await getAllProduct(0, null)
@@ -20,7 +27,7 @@ export default function ProductSection() {
 
 
     useEffect(() => {
-        fetchData()
+        // fetchData()
     }, [])
 
 
@@ -29,17 +36,43 @@ export default function ProductSection() {
         <div>
             
             {/* header */}
-            <div className="w-[25%] mb-8 text-center uppercase bg-sky-500 p-4 text-white rounded-full">
-                Section des produits
-            </div>    
+            <div className="flex flex-row items-center justify-between border-b-2 border-sky-700">
+                <div className="w-[25%] mb-8 text-center uppercase bg-sky-500 p-4 text-white rounded-full">
+                    Section des produits
+                </div>    
+                <div className='w-[23%] text-center pb-4'>
+                        <Tooltip title="Ajouter un categorie">
+                            <button 
+                            onClick={addOperation}
+                            className='w-full bg-sky-600 rounded-md py-2 hover:bg-sky-500 duration-100 cursor-pointer
+                            flex flex-row items-center justify-center space-x-2 text-white'>
+                                <div className='text-xl'>
+                                    Ajouter un produit
+                                </div>
+
+                                <div>
+                                    <AddCircleOutlineIcon sx={{
+                                        fontSize: 25
+                                    }}
+                                    className=''/>
+                                </div>
+                            </button>
+                        </Tooltip>
+                    </div>  
+            </div>
 
             {/* List of product */}
             {(customArray.length > 0) ? 
-                customArray.map((elem, index) => (
-                    <div key={index}>
-                        <ProductComponent />
-                    </div>
-                )
+            (
+
+                <div className="w-full flex flex-col space-y-4 
+                h-[75vh] overflow-y-scroll no-scrollbar">
+                    {customArray.map((elem, index) => (
+                        <div key={index} className="m-0 p-0">
+                            <ProductComponent mainData={"Hello world"}/>
+                        </div>
+                    ))}
+                </div>
             ) : 
                 (
                     <div className='mt-10 text-slate-400 text-center text-xl p-4'>
--- e-commerce-front/src/components/AdminComponents/sections/childCOmponent/ProductComponent.jsx
+++ e-commerce-front/src/components/AdminComponents/sections/childCOmponent/ProductComponent.jsx
@@ -1,14 +1,142 @@
 import { useState, useEffect } from "react"
+import Rating from "@mui/material/Rating";
+import { Product_rating } from "../../../../Hooks/productAPI"; 
+import Tooltip from '@mui/material/Tooltip';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import EditNoteOutlined from "@mui/icons-material/EditNoteOutlined";
+
+export default function ProductComponent ({mainData}) {
+
+    const [data, setData] = useState(null)
+    const [rateValue, setRateValue] = useState(3)
+
+    const sendingRate = async (newValue, id) => {			
+        const response = await Product_rating(5, { rate_value: rateValue });
+        
+        if (response.status === 201) {
+            console.log(response.data);
+            setRateValue(newValue);
+        } else {
+            console.log(response);
+        }
+    };
+
+    const editOperation = async () => {
+        alert("Edit clicked")
+    }
 
 
-export default function ProductComponent () {
+    useEffect(() => {
+        console.log(rateValue, "New value of rate value")
+    }, [rateValue])
 
-    
+    useEffect(() => {
+        setData(mainData)
+    }, [mainData])
 
     return (
 
-        <div>
-            Product component list
+        <div className=" flex flex-row border rounded-md hover:border-slate-400 hover:shadow-md duration-100 border-slate-300 ">
+            {/* Product component list de {data} */}
+            {/* Product image */}
+            <div className="w-[20%] border-r mr-4 basis-1/5">
+                <img
+                    src="./src/assets/yaourt-nature.jpg"
+                    alt="product"
+                    className="produit-img rounded-lg"
+                />
+            </div>
+
+            {/* Product information */}
+            {/* 
+                image *
+                name *
+                description *
+                price *
+                rate *
+                stock
+            */}
+            <div className="flex flex-row justify-around space-x-4 py-2 basis-4/5">
+                <div className="space-y-4 basis-1/3">
+                    <div>
+                        <span className="text-sky-700 font-bold">Nom du produit:</span> 
+                        <span className="text-slate">Socolait Peche</span>
+                    </div>
+
+                    <div>
+                        <span className="text-sky-700 font-bold">Description:</span> 
+                        <span className="text-slate">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus reiciendis optio voluptatem? Explicabo ipsa, incidunt nisi quo itaque optio soluta unde nam. Quibusdam consectetur adipisci velit doloremque ipsa id quae.
+                        </span>
+                    </div>
+                </div>
+
+                <div className="basis-1/3">
+                    <div>
+                        <span className="text-sky-700 font-bold">Prix:</span> 
+                        <span className="text-slate">1.100 ar</span>
+                    </div>
+                    <div className="flex items-center">
+                        <span className="text-sky-700 font-bold">Appreciation:</span> 
+                        <span className="text-slate">
+                        <Rating
+                            name="size-small"
+                            size="medium"
+                            value={rateValue}
+                            onChange={(event, newValue) =>
+                                sendingRate(newValue, data.id)
+                            }
+                        />
+                        </span>
+                    </div>
+                    <div>
+                        <span className="text-sky-700 font-bold">Stock:</span> 
+                        <span className="text-slate"> 50 unite</span>
+                    </div>
+                </div>
+
+                <div className="basis-1/3 flex flex-col justify-center items-center">
+                    <div className='w-[75%] text-center pb-4'>
+                        <Tooltip title="Ajouter un categorie">
+                            <button 
+                            onClick={editOperation}
+                            className='w-full bg-sky-600 rounded-md py-2 hover:bg-sky-500 duration-100 cursor-pointer
+                            flex flex-row items-center justify-center space-x-2 text-white'>
+                                <div className='text-xl'>
+                                    Modifier
+                                </div>
+
+                                <div>
+                                    <EditNoteOutlined sx={{
+                                        fontSize: 25
+                                    }}
+                                    className=''/>
+                                </div>
+                            </button>
+                        </Tooltip>
+                    </div>
+                    
+                    <div className='w-[75%] text-center pb-4'>
+                        <Tooltip title="Ajouter un categorie">
+                            <button 
+                            onClick={editOperation}
+                            className='w-full bg-white rounded-md border border-red-300 text-red-400 py-2 hover:bg-red-400 hover:text-white duration-100 cursor-pointer
+                            flex flex-row items-center justify-center space-x-2'>
+                                <div className='text-xl'>
+                                    Supprimer
+                                </div>
+
+                                <div>
+                                    <AddCircleOutlineIcon sx={{
+                                        fontSize: 25
+                                    }}
+                                    className=''/>
+                                </div>
+                            </button>
+                        </Tooltip>
+                    </div>  
+                </div>
+            </div>
         </div>
 
     )
